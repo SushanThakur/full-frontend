096ff31629652d85d718f9e82b68e2b382a26d14
SushanThakur <mail@sushant.name.np>
2024-11-07 14:10:16 +0545

    added first project

---
+++ 2. 3d Iphone website/src/App.jsx
@@ -0,0 +1,16 @@
+import NavBar from './components/Navbar'
+import Hero from './components/Hero'
+import Highlights from './components/Highlights'
+
+const App = () => {
+
+  return (
+    <main className="bg-black">
+      <NavBar />
+      <Hero />
+      <Highlights />
+    </main>
+  )
+}
+
+export default App
